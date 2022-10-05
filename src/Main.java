d16146366e6bdb9b7b41f5314a132aaa5d9aa334
Sergey Vlasenk0 <vlasenko_1990@internet.ru>
2022-10-05 14:59:16 +0300

    add files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int initialAmount = 500;
+
+        int addAmount = 1800;
+
+        int bonus = addAmount > 1000 ? addAmount / 100 : 0;
+
+
+        int finalAccount = initialAmount + addAmount + bonus;
+        System.out.println("Итоговый счет: " + finalAccount);
+        System.out.println("Бонус: " + bonus);
+
+
     }
 }
